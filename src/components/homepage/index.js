745221f35b0653bfbfc1804163ecc81b9254638e
ankitksh81 <ankitksh81@gmail.com>
2020-11-06 18:17:27 +0530

    added navigation bars

---
+++ src/components/homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../navbars/Navigation';
+
+function Homepage() {
+    return (
+        <div>
+            <Navigation />
+        </div>
+    )
+}
+
+export default Homepage;
