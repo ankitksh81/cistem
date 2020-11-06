2c9b66f44d0d59e0157ad7f8481b79b25b653437
ankitksh81 <ankitksh81@gmail.com>
2020-11-06 23:36:28 +0530

    modified certificate file

--- src/components/certificate/Certificate.jsx
+++ src/components/certificate/Certificate.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet, Image, Font } from '@react-pdf/renderer';
 import playfair from './PlayfairDisplay-Regular.ttf';
+import { emphasize } from '@material-ui/core';
 
 const styles = StyleSheet.create({
   page: {
+    backgroundColor: 'snow',
     flexDirection: 'column',
-    backgroundColor: '#E4E4E4',
     padding: "50px",
     fontFamily: "Helvetica"
   },
   titleText: {
-    fontSize: "40",
+    fontSize: "46",
     fontFamily: "Playfair Display",
     alignSelf:"center",
+    fontWeight: 'heavy',
+  },
+  line: {
+    alignSelf: 'center',
   },
   para1:{
     marginTop: "100px",
@@ -21,18 +26,12 @@ const styles = StyleSheet.create({
   },
   para2:{
     marginTop: "30px",
-    alignSelf:"center"
+    alignSelf:"center",
   },
   name:{
     fontSize: "30",
     alignSelf:"center"
   },
-  signature:{
-    marginTop:"190px",
-    alignSelf:"flex-end",
-    marginRight:"50px",
-    fontSize: "15"
-  }
 });
 
 Font.register({ family: "Playfair Display", fonts: [
@@ -46,6 +45,9 @@ const MyDocument = () => (
       <View style={styles.title}>
         <Text style={styles.titleText}>Certificate of Appreciation</Text>
       </View>
+      <View style={styles.line}>
+        <Text>*--------------------------------------------------------------------------------------------------*</Text>
+      </View>
       <View style={styles.para1}>
         <Text>This is to certify that</Text>
       </View>
@@ -54,9 +56,6 @@ const MyDocument = () => (
       </View>
       <View style={styles.para2}>
         <Text>has bagged the <Text style={{fontWeight:'bold', fontFamily:"Helvetica-Bold"}}>first position</Text> in REVA Hack 2020, a 3 day hackathon conducted by REVA University</Text>
-      </View>
-      <View style={styles.signature}>
-        <Text>Signer1</Text>
       </View>
     </Page>
   </Document>
