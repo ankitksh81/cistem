9903a74dc13d72b5b87f3b924be043062633e22b
ankitksh81 <ankitksh81@gmail.com>
2020-11-07 12:27:02 +0530

    added dashboard

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect} from 'react';
-import './index.css'
+import './Dashboard.css'
 import {Web3Context} from '../../Web3Context';
+import { Button } from '@material-ui/core';
 
 const Dashboard = () => {
     const [web3, portis] = useContext(Web3Context);
@@ -9,7 +10,25 @@ const Dashboard = () => {
     })
     return (
         <div style={{marginTop: "100px", padding:"0px 20px 0px 20px"}}>
-            <h1>Dashboard</h1>
+            <h1 className="dashboard__heading">Dashboard</h1>
+            <div className="dashboard__first">
+                <div className="first__left">
+                    <h2 className="first__heading">
+                        Get Started with&nbsp;<em>CISTEM</em>
+                    </h2>
+                    <p className="first__para">
+                        Somethings you need to setup before you can start using CISTEM.
+                    </p>
+                    <br/>
+
+                </div>
+                <div className="first__right">
+                    <h2 className="first__heading ">
+                        Setup&nbsp;<em>Portis</em>
+                    </h2>
+                    
+                </div>
+            </div>
         </div>
     )
 }
