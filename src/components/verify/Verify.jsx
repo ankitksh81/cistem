4c0d2b63eafb2b7ead8cf2c60ef0a7327c1e07a0
ankitksh81 <ankitksh81@gmail.com>
2020-11-07 15:06:21 +0530

    added veriy cert page

--- src/components/verify/Verify.jsx
+++ src/components/verify/Verify.jsx
@@ -1,13 +1,23 @@
-import { Typography } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 import './Verify.css';
 
 const Verify = () => {
     return (
-        <div className="container">
-            <Typography variant="h2" className="heading">Verify Certificate</Typography>
+        <div className="verify__container">
+            <div className="verify__heading">
+                <h2 className="heading">Verify Certificate</h2>
+            </div>
+            <div className="verify__input">
+                <TextField className="hash__input" id="outlined-basic" label="Certificate Hash" variant="outlined" />
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <Button variant="contained" size="large" type="submit" color="primary" className="very__button">Verify</Button>
         </div>
     )
 }
 
-export default Verify
+export default Verify;
+
